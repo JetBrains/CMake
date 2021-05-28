12d1afd68dc7f4b326a388a00808a6c553455fb0
Sysprogs <sysprogs@sysprogs.com>
2021-05-28 16:22:14 -0700

    Added the CMake debugger

--- Source/cmake.h
+++ Source/cmake.h
@@ -35,6 +35,8 @@
 
 #  include "cmCMakePresetsGraph.h"
 #  include "cmMakefileProfilingData.h"
+
+#  include "Sysprogs/HLDPServer.h"
 #endif
 
 class cmConfigureLog;
@@ -197,6 +199,12 @@ public:
   void SetHomeOutputDirectory(const std::string& dir);
   std::string const& GetHomeOutputDirectory() const;
   //@}
+
+#ifndef CMAKE_BOOTSTRAP
+  void StartDebugServerIfEnabled();
+  void StopDebugServerIfNeeded();
+  Sysprogs::HLDPServer* GetDebugServer() { return m_pDebugServer.get(); }
+#endif
 
   /**
    * Working directory at CMake launch
@@ -639,6 +647,9 @@ public:
   cmFileAPI* GetFileAPI() const { return this->FileAPI.get(); }
 #endif
 
+  unsigned GetDebugServerPort() const { return DebugServerPort; }
+  void SetDebugServerPort(unsigned port) { DebugServerPort = port; }
+
   cmState* GetState() const { return this->State.get(); }
   void SetCurrentSnapshot(cmStateSnapshot const& snapshot)
   {
@@ -707,6 +718,11 @@ protected:
   std::string GeneratorInstance;
   std::string GeneratorPlatform;
   std::string GeneratorToolset;
+  unsigned DebugServerPort = 0;
+#ifndef CMAKE_BOOTSTRAP
+  std::unique_ptr<Sysprogs::HLDPServer> m_pDebugServer;
+#endif
+  
   bool GeneratorInstanceSet = false;
   bool GeneratorPlatformSet = false;
   bool GeneratorToolsetSet = false;
