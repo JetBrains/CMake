de6dc48b881dc52989c05348ce4cc42ca7198825
Sysprogs <sysprogs@sysprogs.com>
2021-05-28 16:22:14 -0700

    Added the CMake debugger

--- Source/cmGlobalGenerator.cxx
+++ Source/cmGlobalGenerator.cxx
@@ -1385,6 +1385,9 @@ void cmGlobalGenerator::Configure()
 
   // now do it
   this->ConfigureDoneCMP0026AndCMP0024 = false;
+#ifndef CMAKE_BOOTSTRAP
+  this->CMakeInstance->StartDebugServerIfEnabled();
+#endif  
   dirMf->Configure();
   dirMf->EnforceDirectoryLevelRules();
 
