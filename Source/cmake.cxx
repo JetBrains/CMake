d16bfd1ea155f28b79622a68e88b37fbe7949d18
Dmitry.Neverov <dmitry.neverov@jetbrains.com>
2022-09-26 09:55:57 +0200

    [dbg] check debug token during handshake

--- Source/cmake.cxx
+++ Source/cmake.cxx
@@ -1134,6 +1134,13 @@ void cmake::SetArgs(const std::vector<std::string>& args)
                        state->SetDebugServerPort(atoi(value.c_str()));
                        return true;
                      } },
+
+    CommandArgument{ "--debug-token-path", CommandArgument::Values::One,
+                     [](std::string const& value, cmake* state) -> bool {
+                       state->SetDebugServerTokenPath(value);
+                       return true;
+                     } },
+
     CommandArgument{
       "--debug-find", CommandArgument::Values::Zero,
       [](std::string const&, cmake* state) -> bool {
@@ -4200,7 +4207,7 @@ bool cmake::GetDeprecatedWarningsAsErrors() const
 void cmake::StartDebugServerIfEnabled()
 {
   if (!m_pDebugServer && DebugServerPort) {
-    m_pDebugServer.reset(new Sysprogs::HLDPServer(DebugServerPort));
+    m_pDebugServer.reset(new Sysprogs::HLDPServer(DebugServerPort, DebugServerTokenPath));
     if (!m_pDebugServer->WaitForClient()) {
       cmSystemTools::Error("Failed to start debugging server. Aborting...");
       cmSystemTools::SetFatalErrorOccurred();
