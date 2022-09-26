d8248520509dbfdfc7c33be119eb66eb2d0a666f
Dmitry.Neverov <dmitry.neverov@jetbrains.com>
2022-09-26 09:55:57 +0200

    [dbg] check debug token during handshake

--- Source/cmake.h
+++ Source/cmake.h
@@ -657,6 +657,7 @@ public:
 
   unsigned GetDebugServerPort() const { return DebugServerPort; }
   void SetDebugServerPort(unsigned port) { DebugServerPort = port; }
+  void SetDebugServerTokenPath(std::string const& debugTokenPath) { DebugServerTokenPath = debugTokenPath; }
 
   cmState* GetState() const { return this->State.get(); }
   void SetCurrentSnapshot(cmStateSnapshot const& snapshot)
@@ -727,6 +728,7 @@ protected:
   std::string GeneratorPlatform;
   std::string GeneratorToolset;
   unsigned DebugServerPort = 0;
+  std::string DebugServerTokenPath;
 #ifndef CMAKE_BOOTSTRAP
   std::unique_ptr<Sysprogs::HLDPServer> m_pDebugServer;
 #endif
